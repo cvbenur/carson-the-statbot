6ececba67ad7ea95c666b862fe8030ddf1024613
R-Nbt <rubenabet@gmail.com>
2020-04-08 14:00:39 +0200

    Add handler

--- js/handler/command.js
+++ js/handler/command.js
@@ -1,1 +1,27 @@
 const { readdirSync } = require('fs');
+
+const ascii = require('ascii-table');
+
+const table = new ascii().setHeading("Command", "Load status");
+
+module.exports = (client) => {
+    readdirSync("./commands/").forEach(dir => {
+        const commands = readdirSync(`./commands/${dir}`).filter(f => f.endsWith(".js"));
+
+        for (let file of commands) {
+            let pull = require(`../commands/${dir}/${file}`);
+
+            if (pull.name) {
+                client.command.set(pull.name, pull);
+                table.addRow(file, '✔️');
+            } else {
+                table.addRow(file, '❌ -> missing file.');
+                continue;
+            }
+
+            if (pull.aliases && Array.isArray(pull)) {
+                pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
+            }
+        }
+    });
+}
