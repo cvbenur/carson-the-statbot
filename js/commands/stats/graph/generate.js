55846d1f1b225829c728af9e814b4db88f02db75
R-Nbt <rubenabet@gmail.com>
2020-04-28 15:56:56 +0200

    Add successful graph-to-SVG generation

--- js/commands/stats/graph/generate.js
+++ js/commands/stats/graph/generate.js
@@ -1,13 +1,20 @@
-const { writeFile, unlink } = require('fs');
+const { writeFileSync, unlink, writeFile } = require('fs');
+const vega = require('vega');
 
 
 
 // Delete graph file if it already exists
-function removeExistingFile (name) {
+async function removeExistingFile (name) {
+
+    switch (name.split('.')[1]) {
+        case 'svg': name = `svg/${name}`;
+        case 'png': name = `png/${name}`;
+        default: console.log(`Invalid file extension : ${name}`);
+    }
     
-    unlink(`../../../../generated/${name}.png`, (err) => {
+    unlink(`./assets/generated/${name}`, (err) => {
         if(err && err.code == 'ENOENT') {
-            console.info("File doesn't exist.");
+            console.info("File doesn't exist, cannot remove it.");
         } else if (err) {
             console.error("Error occurred while trying to remove file");
         } else {
@@ -23,22 +30,51 @@ function generateHexString (length) {
 }
 
 
+// Write SVG string to file
+function writeSVGToFile (svgString, svgName) {
+
+    writeFile(`./assets/generated/svg/${svgName}`, svgString, (err) => {
+
+        if (err) {
+            // Logging errors
+            console.error(err);
+        } else console.log(`SVG written to file : '${svgName}'`)
+    });
+}
+
+
 // Render image from given graph
 async function graphToImage (statsObject) {
 
-
     // Generate a new 10-char hex string
-    const pngName = generateHexString(10);
-
-    removeExistingFile(pngName);
+    const imgName = generateHexString(10);
 
 
-    // Convert stats object to graph
+    var view = new vega.View(vega.parse(statsObject), {renderer: 'none'});
+
+
+    // generate a static PNG image
+    view.toSVG().then(async function (svg) {
+
+        // Working SVG string
+        //console.log(svg);
+
+        // Write SVG to file
+        writeSVGToFile(svg, imgName + '.svg');
+
+
+        // WRITE SVG FILE TO PNG FILE
+        
+
+    }).catch(function(err) {
+
+        console.error(err);
+
+    });
+
     
-    // Generate image and save file to PNG in the "../../../../" directory
-
-    
-    return pngName;
+    // Return the name of the generated PNG file
+    return `${imgName}.png`;
 }
 
 
