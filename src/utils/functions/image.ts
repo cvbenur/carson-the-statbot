5f4d34f3b91acfe9d99a6b702b219cc49dd0ce14
R-Nbt <rubenabet@gmail.com>
2020-08-27 19:47:03 +0200

    Add image generation functions

--- src/utils/functions/image.ts
+++ src/utils/functions/image.ts
@@ -1,8 +1,10 @@
 import { unlink } from 'fs';
+import vega = require('vega');
+import sharp = require('sharp');
 
 
 // Remove existing generated file
-export function removeExistingImage(name: string): void {
+function removeExistingImage(name: string): void {
   let locName = '';
 
   // Check the file's extension
@@ -26,4 +28,38 @@ export function removeExistingImage(name: string): void {
       );
     } else console.info(`Removed file : '${locName}'`);
   });
+}
+
+// Generate a random hexa string of given length
+function generateHexString(length: number): string {
+  return [...Array(length)]
+    .map(() => Math.floor(Math.random() * 16).toString(16))
+    .join("");
+}
+
+// Render image from given graph
+function graphToImage(graphObject): string {
+
+  const pngName = `${generateHexString(10)}.png`;
+  removeExistingImage(pngName);
+
+  const view = new vega.View(vega.parse(graphObject), {renderer: 'none'});
+
+  view.toSVG().then(async (svg) => {
+
+    await sharp(Buffer.from(svg))
+      .toFormat('png')
+      .toFile(pngName);
+
+  }).catch(function(err) {
+      console.error(err);
+  });
+
+  return pngName;
+}
+
+
+// Returns name of generated PNG image
+export function generatePNGFromGraph(graphObject): string {
+  return graphToImage(graphObject);
 }
